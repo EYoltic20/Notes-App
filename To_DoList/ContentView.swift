1caeb67c1b42ea9b9d080e056d5a5ce134d4296d
Yoltic2 <eyolticmg@icloud.com>
2022-03-15 09:55:43 -0600

    Initial Commit

---
+++ To_DoList/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  To_DoList
+//
+//  Created by Emilio Y Martinez on 15/03/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ To_DoList/To_DoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  To_DoListApp.swift
+//  To_DoList
+//
+//  Created by Emilio Y Martinez on 15/03/22.
+//
+
+import SwiftUI
+
+@main
+struct To_DoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
